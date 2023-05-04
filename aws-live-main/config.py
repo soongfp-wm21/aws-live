491b528f00f0c7edd341119adf1ec1056bf82025
soongfp-wm21 <132387916+soongfp-wm21@users.noreply.github.com>
2023-05-05 00:46:15 +0800

    Update config.py

--- aws-live-main/config.py
+++ aws-live-main/config.py
@@ -1,4 +1,4 @@
-customhost = "airasiardsdb1.cfdqvky8a8nn.us-east-1.rds.amazonaws.com"
+customhost = "airasiardsinstance.cfdqvky8a8nn.us-east-1.rds.amazonaws.com"
 customuser = "admin"
 custompass = "123123123"
 customdb = "airAsia"
